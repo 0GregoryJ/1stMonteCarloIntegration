e94a1970c3dd13e56df5d5b38bc215da8290b27c
0GregoryJ <161426345+0GregoryJ@users.noreply.github.com>
2024-02-27 10:59:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import math
 import random
 import os;
 
-
 print('Increasing on interval function integrator:')
+
+#define desired function to integrate (must be increasing on desired interval)
 def function(x):
     return math.exp(x)
     
@@ -21,6 +22,7 @@ x_blim = float(x_blim)
 count = 0.0
 in_area = 0.0
 
+#plots n dots
 while count < int(n):
     x_coord = random.uniform(x_blim, x_ulim)
     y_coord = random.uniform(function(x_blim),function(x_ulim))
@@ -30,5 +32,6 @@ while count < int(n):
     
     count += 1
 
+#counts how many dots fall within region below graph
 ans = (in_area / count)*((x_ulim-x_blim)*(function(x_ulim))-function(x_blim))
 print(ans)
